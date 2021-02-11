179c980d3cf45a99a7c6fa923b49444c10766dbf
wissemkhrarib <khraribwissem@gmail.com>
2021-02-11 01:40:01 -0600

    product model created

---
+++ products/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    description = models.CharField(max_length=2000)
