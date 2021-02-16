e1748b73c3fbc8396bc58837e6b086b75b321361
wissemkhrarib <khraribwissem@gmail.com>
2021-02-16 11:58:05 -0600

    products_list view added

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from django.http import JsonResponse, HttpResponse
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+
+def products_list(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return JsonResponse(data=serializer.data, safe=False)
+
+    #if request.method == 'POST':
