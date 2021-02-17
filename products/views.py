0a99b26f02848a191135508fb7fe47b982f85230
wissemkhrarib <khraribwissem@gmail.com>
2021-02-17 04:09:39 -0600

    post method added

--- products/views.py
+++ products/views.py
@@ -2,12 +2,20 @@ from .models import Product
 from .serializers import ProductSerializer
 from django.http import JsonResponse, HttpResponse
 from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def products_list(request):
     if request.method == 'GET':
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return JsonResponse(data=serializer.data, safe=False)
 
-    #if request.method == 'POST':
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ProductSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
